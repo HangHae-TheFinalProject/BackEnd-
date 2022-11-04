9abd956abba8b1d67355c813305d1fcfadc6f51d
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-11-05 00:30:00 +0900

    jpaquery 테스트

--- src/main/java/com/example/finalproject/service/MemberService.java
+++ src/main/java/com/example/finalproject/service/MemberService.java
@@ -2,8 +2,11 @@ package com.example.finalproject.service;
 
 import com.example.finalproject.controller.request.MemberRequestDto;
 import com.example.finalproject.exception.PrivateResponseBody;
+import com.example.finalproject.exception.StatusCode;
 import com.example.finalproject.repository.MemberRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +14,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberService {
 
+    private final JPAQueryFactory jpaQueryFactory;
     private final MemberRepository memberRepository;
 
-//    public ResponseEntity<PrivateResponseBody> signup(MemberRequestDto memberRequestDto){
-//
-//
-//
-//        return new ResponseEntity<>(new PrivateResponseBody());
-//    }
+    public ResponseEntity<PrivateResponseBody> signup(MemberRequestDto memberRequestDto){
+
+
+
+        return new ResponseEntity<>(new PrivateResponseBody
+                (StatusCode.OK,"회원가입 성공"), HttpStatus.OK);
+    }
 
 }
