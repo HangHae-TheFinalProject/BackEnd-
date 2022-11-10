0d5d22f63e1d391ed2a8a29f2aa5bcbc2d5b529d
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-11-11 04:08:28 +0900

    Fix : 입장 알림 채팅 기능 오류 수정

--- src/main/java/com/example/finalproject/service/ChatService.java
+++ src/main/java/com/example/finalproject/service/ChatService.java
@@ -39,14 +39,14 @@ public class ChatService {
 //    }
 public void meesage(ChatMessage message) {
 
-    String nickname = "test";
-    // 로그인 회원 정보로 대화명 설정
-    message.setSender(nickname);
+//    String nickname = "test";
+//    // 로그인 회원 정보로 대화명 설정
+//    message.setSender(nickname);
 //    message.setRoomId("97a853e7-73d1-41a2-a380-71b28e07b02e");
     // 채팅방 입장시에는 대화명과 메시지를 자동으로 세팅한다.
     if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-        message.setSender("[알림]");
-        message.setMessage(nickname + "님이 입장하셨습니다.");
+//        message.setSender("[알림]");
+        message.setMessage(message.getSender() + "님이 입장하셨습니다.");
     }
     // Websocket에 발행된 메시지를 redis로 발행(publish)
     redisTemplate.convertAndSend(channelTopic.getTopic(), message);
