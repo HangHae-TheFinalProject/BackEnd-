02c3fdeab1f3c4d37b5f4ae0bae15f2f1bc8b51a
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-11-15 21:18:26 +0900

    Feat : Google Q 객체 일시 주석처리

--- src/main/java/com/example/finalproject/service/MemberService.java
+++ src/main/java/com/example/finalproject/service/MemberService.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Optional;
 
-import static com.example.finalproject.domain.QMember.member;
+//import static com.example.finalproject.domain.QMember.member;
 
 @Slf4j
 @RequiredArgsConstructor
