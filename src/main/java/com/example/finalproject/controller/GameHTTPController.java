fd2517de5f73ad6ef6230b963b6216be5806220e
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-11-18 16:05:55 +0900

    Fix : Game 후반부 로직 소켓 연결로 수정

--- src/main/java/com/example/finalproject/controller/GameHTTPController.java
+++ src/main/java/com/example/finalproject/controller/GameHTTPController.java
@@ -1,91 +1,40 @@
 package com.example.finalproject.controller;
 
-import com.example.finalproject.controller.request.StringDto;
-import com.example.finalproject.exception.PrivateResponseBody;
-import com.example.finalproject.exception.StatusCode;
+
 import com.example.finalproject.service.GameHTTPService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/lier/manager")
 public class GameHTTPController {
-
-    /*
-        구현 해야 할 api
-        - 투표하기
-        - 라이어 정답 맞추기
-        - 얘가 라이어가 맞나요 isLier
-        - 한 바퀴 더
-            0,1 할건지 말건지 -> 동점시 다 말할때
-    */
-    /*
-        구현 해야 할 api
-        - 한 바퀴 더
-    */
 
     private final GameHTTPService gameHTTPService;
 
-    @PostMapping("/{gameroomid}/isLier") // 프론트에서 라이어가 맞는지 확인할 때 사용. token필요 x
-    @ResponseBody
-    public Boolean isLier(@PathVariable Long gameroomid, @RequestBody StringDto stringDto) {
-        return gameHTTPService.isLier(gameroomid, stringDto);
-    }
-
-    @PostMapping("/{gameroomid}/vote")
-    @ResponseBody
-    public ResponseEntity<PrivateResponseBody> vote(@PathVariable Long gameroomid, @RequestBody StringDto stringDto/*, HttpServletRequest request*/) {
-        return gameHTTPService.vote(gameroomid, stringDto);
-    }
-    @PostMapping("/{gameroomid}/isAnswer") // 정답 맞는지 확인할 때 사용. token필요 x
-    @ResponseBody
-    public Boolean isAnswer(@PathVariable Long gameroomid, @RequestBody StringDto stringDto) {
-        return gameHTTPService.isAnswer(gameroomid, stringDto);
-    }
-
-    @GetMapping("/{gameroomid}/oneMoerRound")
-    @ResponseBody
-    public ResponseEntity<PrivateResponseBody> oneMoerRound(@PathVariable Long gameroomid) {
-        return gameHTTPService.oneMoerRound(gameroomid);
-    }
-
-    @GetMapping("/{gameroomid}/isRound") // 한바퀴 더 가능 여부 확인할 때 사용. token필요 x
-    @ResponseBody
-    public Boolean isRound(@PathVariable Long gameroomid) {
-        return gameHTTPService.isRound(gameroomid);
-    }
-
-
-    /////////////////// test
-    @PostMapping("/isLiertest") // 프론트에서 라이어가 맞는지 확인할 때 사용. token필요 x
-    @ResponseBody
-    public Boolean isLiertest(@RequestBody StringDto stringDto) {
-        return gameHTTPService.isLiertest(stringDto);
-    }
-
-    @PostMapping("/{num}/votetest")
-    @ResponseBody
-    public ResponseEntity<PrivateResponseBody> votetest(@PathVariable int num, @RequestBody StringDto voteDto/*, HttpServletRequest request*/) {
-        return gameHTTPService.votetest(num, voteDto);
-    }
-
-    @GetMapping("/oneMoerRoundtest")
-    @ResponseBody
-    public ResponseEntity<PrivateResponseBody> oneMoerRoundtest() {
-        Integer test = 3;
-        return new ResponseEntity<>(new PrivateResponseBody
-                (StatusCode.OK, test), HttpStatus.OK);
-    }
-
-//    @PostMapping("/test")
-//    @ResponseBody
-//    public ResponseEntity<PrivateResponseBody> test(@RequestBody String name) {
-//        System.out.println(name);
-//        return new ResponseEntity<>(new PrivateResponseBody
-//                (StatusCode.OK, null), HttpStatus.CONTINUE);
+//    @MessageMapping("/lier/manager/{gameroomid}/isLier/{name}")
+//    public void isLier(@DestinationVariable("gameroomid") Long gameroomid,@DestinationVariable("name") String name) {
+//        gameHTTPService.isLier(gameroomid, name);
 //    }
+
+    @MessageMapping("/lier/manager/{gameroomid}/vote/{name}")
+    public void vote(@DestinationVariable("gameroomid") Long gameroomid, @DestinationVariable("name") String name) {
+        gameHTTPService.vote(gameroomid, name);
+    }
+    @MessageMapping("/lier/manager/{gameroomid}/isAnswer/{answer}") // 정답 맞는지 확인할 때 사용. token필요 x
+    public void isAnswer(@DestinationVariable("gameroomid") Long gameroomid, @DestinationVariable("answer") String answer) {
+        gameHTTPService.isAnswer(gameroomid, answer);
+    }
+
+    @MessageMapping("/lier/manager/{gameroomid}/oneMoerRound")
+    public void oneMoerRound(@DestinationVariable("gameroomid") Long gameroomid) {
+        gameHTTPService.oneMoerRound(gameroomid);
+    }
+
+    @MessageMapping("/lier/manager/{gameroomid}/victory")
+    public void victory(@DestinationVariable("gameroomid") Long gameroomid) {
+        gameHTTPService.victory(gameroomid);
+    }
 }
--- src/main/java/com/example/finalproject/service/GameHTTPService.java
+++ src/main/java/com/example/finalproject/service/GameHTTPService.java
@@ -1,17 +1,23 @@
 package com.example.finalproject.service;
 
 import com.example.finalproject.controller.request.StringDto;
+import com.example.finalproject.domain.GameMessage;
 import com.example.finalproject.domain.GameRoomMember;
 import com.example.finalproject.domain.GameStartSet;
+import com.example.finalproject.domain.Member;
 import com.example.finalproject.exception.PrivateResponseBody;
 import com.example.finalproject.exception.StatusCode;
+import com.example.finalproject.jwt.TokenProvider;
 import com.example.finalproject.repository.GameStartSetRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -21,46 +27,83 @@ import static com.example.finalproject.domain.QGameRoomMember.gameRoomMember;
 import java.util.Map.Entry;
 import java.util.ArrayList;
 import java.util.Comparator;
-
+import static com.example.finalproject.domain.QMember.member;
 @RequiredArgsConstructor
 @Service
 public class GameHTTPService {
 
     private final GameStartSetRepository gameStartSetRepository;
+    private final SimpMessageSendingOperations messagingTemplate;
     private final JPAQueryFactory jpaQueryFactory;
+    private final TokenProvider jwtTokenProvider;
     static int cnt =0;
-//    static int test =0;
     static HashMap<String, Integer> voteHashMap = new HashMap<>();
-    public Boolean isLier(Long gameroomid, StringDto stringDto) {
-        String nickname = stringDto.getValue();
-        GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
-        return gameStartSet.getLier().equals(nickname);
-    }
-    public ResponseEntity<PrivateResponseBody> vote(Long gameroomid, StringDto stringDto){
-        String name = stringDto.getValue();
+
+//    public void isLier(Long gameroomid, String name) {
+//        GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
+//
+//        GameMessage gameMessage = new GameMessage();
+//        gameMessage.setRoomId(Long.toString(gameroomid));
+//        gameMessage.setSenderId("");
+//        gameMessage.setSender("");
+//        gameMessage.setContent(gameStartSet.getLier().equals(name) +"");
+//        gameMessage.setType(GameMessage.MessageType.START);
+//        messagingTemplate.convertAndSend("/sub/gameroom/" + gameroomid, gameMessage);
+//    }
+    @Transactional
+    public void vote(Long gameroomid, String name){
         voteHashMap.put(name, voteHashMap.getOrDefault(name, 0) + 1);
         cnt ++;
 
-//        GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
+        GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
+
         List<GameRoomMember> gameRoomMembers = jpaQueryFactory
                 .selectFrom(gameRoomMember)
                 .where(gameRoomMember.gameroom_id.eq(gameroomid))
                 .fetch();
         int memberNum = gameRoomMembers.size();
-        if(cnt == memberNum){
-            return sortHash();
+        if(cnt != memberNum){
+            GameMessage gameMessage = new GameMessage();
+            gameMessage.setRoomId(Long.toString(gameroomid));
+            gameMessage.setSenderId("");
+            gameMessage.setSender("");
+            gameMessage.setContent("투표중 입니다.");
+            gameMessage.setType(GameMessage.MessageType.START);
+            messagingTemplate.convertAndSend("/sub/gameroom/" + gameroomid, gameMessage);
+            return;
         }
-        return new ResponseEntity<>(new PrivateResponseBody
-                (StatusCode.OK, null), HttpStatus.CONTINUE);
-    }
+        // 투표가 끝나면
+        List<String> nickName = sortHash();
+        String content = "";
+        if(nickName.size()==1){
+            if(nickName.get(0).equals(gameStartSet.getLier())){
+                content ="는 라이어가 맞습니다.";
+            }
+            else{
+                content = "는 라이어가 아닙니다.";
+                gameStartSet.setWinner(GameStartSet.Winner.LIER);
+            }
+        }
+        else{
+            if (gameStartSet.getRound()<4){
+                content = "동점입니다.";
+            }
+            else{
+                content ="동점 입니다. 라이어의 승리 입니다. 라이어는 " +gameStartSet.getLier() + "입니다.";
+                gameStartSet.setWinner(GameStartSet.Winner.LIER);
+            }
+        }
 
-    public Boolean isAnswer(Long gameroomid, StringDto stringDto) {
-        String answer = stringDto.getValue();
-        GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
-        return gameStartSet.getKeyword().equals(answer);
-    }
+        GameMessage gameMessage = new GameMessage();
+        gameMessage.setRoomId(Long.toString(gameroomid));
+        gameMessage.setSenderId("");
+        gameMessage.setSender("");
+        gameMessage.setContent(nickName +content +"");
+        gameMessage.setType(GameMessage.MessageType.START);
+        messagingTemplate.convertAndSend("/sub/gameroom/" + gameroomid, gameMessage);
 
-    public ResponseEntity<PrivateResponseBody> sortHash(){
+    }
+    public List<String> sortHash(){
         List<Entry<String, Integer>> list_entries = new ArrayList<Entry<String, Integer>>(voteHashMap.entrySet());
 
         // 비교함수 Comparator를 사용하여 내림 차순으로 정렬
@@ -85,72 +128,95 @@ public class GameHTTPService {
         }
         voteHashMap.clear();
         cnt=0;
-        return new ResponseEntity<>(new PrivateResponseBody
-                (StatusCode.OK, nickName), HttpStatus.OK);
+        return nickName;
     }
 
-    public ResponseEntity<PrivateResponseBody> oneMoerRound(Long gameroomid) {
+    @Transactional
+    public void isAnswer(Long gameroomid, String answer) {
         GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
-        return new ResponseEntity<>(new PrivateResponseBody
-                (StatusCode.OK, gameStartSet.oneMoerRound()), HttpStatus.OK);
-    }
+        GameMessage gameMessage = new GameMessage();
+        gameMessage.setRoomId(Long.toString(gameroomid));
+        gameMessage.setSenderId("");
+        gameMessage.setSender("");
+        gameMessage.setContent(gameStartSet.getKeyword().equals(answer) +"");
+        gameMessage.setType(GameMessage.MessageType.START);
+        messagingTemplate.convertAndSend("/sub/gameroom/" + gameroomid, gameMessage);
 
-    public Boolean isRound(Long gameroomid) {
-        GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
-        return gameStartSet.getRound()<4;
-    }
-
-
-    ///////////////test
-    public Boolean isLiertest(StringDto stringDto) {
-        String nickname = stringDto.getValue();
-        return "amy".equals(nickname);
-    }
-
-    public ResponseEntity<PrivateResponseBody> votetest(int num, StringDto stringDto){
-        String name = stringDto.getValue();
-        voteHashMap.put(name, voteHashMap.getOrDefault(name, 0) + 1);
-        System.out.println(name + " : "+voteHashMap.get(name));
-        cnt ++;
-        if(cnt == num){
-            return sortHash();
+        if(gameStartSet.getKeyword().equals(answer)) {// 라이어가 정답을 맞추면
+            gameStartSet.setWinner(GameStartSet.Winner.LIER);
         }
-        return new ResponseEntity<>(new PrivateResponseBody
-                (StatusCode.OK, null), HttpStatus.OK);
+        else {
+            gameStartSet.setWinner(GameStartSet.Winner.CITIZEN);
+        }
     }
 
+    @Transactional
+    public void oneMoerRound(Long gameroomid) {
+        GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
+        Integer round = gameStartSet.oneMoerRound();
+        GameMessage gameMessage = new GameMessage();
+        gameMessage.setRoomId(Long.toString(gameroomid));
+        gameMessage.setSenderId("");
+        gameMessage.setSender("");
+        gameMessage.setContent("Round : " + round);
+        gameMessage.setType(GameMessage.MessageType.START);
+        messagingTemplate.convertAndSend("/sub/gameroom/" + gameroomid, gameMessage);
 
-//    public void test(){
-//        Map<String, Integer> testMap = new HashMap<String, Integer>();
-//
-//        // Map에 데이터 추가
-//        testMap.put( "apple", 1);
-//        testMap.put( "orange", 2);
-//        testMap.put( "pineapple", 4);
-//        testMap.put( "strawberry", 5);
-//        testMap.put( "melon", 3);
-//
-//        // Map.Entry 리스트 작성
-//        List<Entry<String, Integer>> list_entries = new ArrayList<Entry<String, Integer>>(testMap.entrySet());
-//
-//        // 비교함수 Comparator를 사용하여 내림 차순으로 정렬
-//        Collections.sort(list_entries, new Comparator<Entry<String, Integer>>() {
-//            // compare로 값을 비교
-//            public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2)
-//            {
-//                // 내림 차순으로 정렬
-//                return obj2.getValue().compareTo(obj1.getValue());
-//            }
-//        });
-//
-//        System.out.println("내림 차순 정렬");
-//        // 결과 출력
-//        for(Entry<String, Integer> entry : list_entries) {
-//            System.out.println(entry.getKey() + " : " + entry.getValue());
-//        }
-//
-////        System.out.println(test);
-////        test++;
-//////        if(test == 2) test =0;
-//    }
+    }
+    @Transactional
+    public void victory(Long gameroomid){
+        GameStartSet gameStartSet= gameStartSetRepository.findByRoomId(gameroomid);
+        System.out.println(gameStartSet.getGamestartsetId());
+
+        List<GameRoomMember> gameRoomMembers = jpaQueryFactory
+                .selectFrom(gameRoomMember)
+                .where(gameRoomMember.gameroom_id.eq(gameroomid))
+                .fetch();
+
+        List<Member> playingMembers = new ArrayList<>();
+
+        for(GameRoomMember gameRoomMember2 : gameRoomMembers){
+
+            Member each_member = jpaQueryFactory
+                    .selectFrom(member)
+                    .where(member.memberId.eq(gameRoomMember2.getMember_id()))
+                    .fetchOne();
+
+            playingMembers.add(each_member);
+        }
+        List<String> winner = new ArrayList<>();
+        List<String> loser = new ArrayList<>();
+        if(gameStartSet.getWinner().equals(GameStartSet.Winner.LIER)){
+            for(Member playingMember : playingMembers){
+                if(playingMember.getNickname().equals(gameStartSet.getLier())){
+                    playingMember.addWin();
+                    winner.add(playingMember.getNickname());
+                }
+                else {
+                    playingMember.addLose();
+                    loser.add(playingMember.getNickname());
+                }
+            }
+        }
+        else{
+            for(Member playingMember : playingMembers){
+                if(playingMember.getNickname().equals(gameStartSet.getLier())){
+                    playingMember.addLose();
+                    loser.add(playingMember.getNickname());
+                }
+                else {
+                    playingMember.addWin();
+                    winner.add(playingMember.getNickname());
+                }
+            }
+        }
+        GameMessage gameMessage = new GameMessage();
+        gameMessage.setRoomId(Long.toString(gameroomid));
+        gameMessage.setSenderId("");
+        gameMessage.setSender("");
+        gameMessage.setContent("Winner : "+winner +" Loser : " + loser);
+        gameMessage.setType(GameMessage.MessageType.START);
+        messagingTemplate.convertAndSend("/sub/gameroom/" + gameroomid, gameMessage);
+
+    }
 }
