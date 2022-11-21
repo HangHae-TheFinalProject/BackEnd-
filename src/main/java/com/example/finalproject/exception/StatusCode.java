41e19c6d74468a916b4e44e47e0fb082d3f8a212
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-11-21 10:29:00 +0900

    dev - 일부 코드 수정

--- src/main/java/com/example/finalproject/exception/StatusCode.java
+++ src/main/java/com/example/finalproject/exception/StatusCode.java
@@ -16,8 +16,8 @@ public enum StatusCode {
     LOGIN_WRONG_FORM_JWT_TOKEN(HttpStatus.BAD_REQUEST, "115", "JWT 토큰이 잘못되었습니다."),
     LOGIN_MEMBER_REQUIRED_INFORMATION_FAIL(HttpStatus.BAD_REQUEST, "116", "필수 입력 정보를 입력 후 시도해주세요"),
     NOT_MATCH_POST(HttpStatus.BAD_REQUEST, "117", "현재 로그인한 유저가 작성한 게시글이 아닙니다."),
-    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST,"117","중복된 email이 있습니다."),
-    DUPLICATED_PASSWORD(HttpStatus.BAD_REQUEST,"118","Password가 틀립니다."),
+    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST,"117","이미 존재하는 닉네임입니다."),
+    DUPLICATED_PASSWORD(HttpStatus.BAD_REQUEST,"118","비밀번호가 틀립니다."),
     POST_ERROR(HttpStatus.BAD_REQUEST,"119","게시글 작성이 필요합니다."),
     NOT_EXIST_MEDIA(HttpStatus.BAD_REQUEST,"120","이미지가 존재하지 않아 게시글 작성이 불가합니다."),
     NOT_FOUND_ROOM(HttpStatus.BAD_REQUEST,"121","입장할 방이 존재하지 않습니다."),
@@ -27,6 +27,8 @@ public enum StatusCode {
     ALREADY_PLAYING(HttpStatus.BAD_REQUEST,"125","게임 진행 중인 방에는 입장할 수 없습니다."),
     NOT_MATCH_PLAYER(HttpStatus.BAD_REQUEST,"126","해당 방의 참가자가 아니라 준비를 진행할 수 없습니다."),
     NOT_READY(HttpStatus.BAD_REQUEST,"127","모든 인원이 준비완료 상태가 아니라 게임을 시작할 수 없습니다."),
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST,"128","이미 존재하는 이메일입니다."),
+    ROOMNAME_OVER(HttpStatus.BAD_REQUEST,"129","생성할 수 있는 방 이름 글자 수가 초과하였습니다."),
 
     INTERNAL_SERVER_ERROR_PLZ_CHECK(HttpStatus.INTERNAL_SERVER_ERROR, "999", "알수없는 서버 내부 에러 발생 , dladlsgur3334@gmail.com 으로 연락 부탁드립니다.");
 
