8bbaf194e956018eb2bb2784483d5b752eaed050
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-11-22 12:57:57 +0900

    Feat: Comment

--- src/main/java/com/example/finalproject/domain/Post.java
+++ src/main/java/com/example/finalproject/domain/Post.java
@@ -38,5 +38,8 @@ public class Post {
     @OneToMany(mappedBy = "post",fetch = FetchType.LAZY,cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Media> medias;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
 
 }
