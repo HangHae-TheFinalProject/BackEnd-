b7288f1e47b8d280796a6a8f897747b6f55f637b
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-11-25 21:20:44 +0900

    Fix : Post - 게시글 생성일자 형식 수정 - Openvidu 수정

--- src/main/java/com/example/finalproject/service/GameRoomService.java
+++ src/main/java/com/example/finalproject/service/GameRoomService.java
@@ -181,7 +181,8 @@ public class GameRoomService {
     public ResponseEntity<?> makeGameRoom(
             GameRoomRequestDto gameRoomRequestDto, // 방 생성을 위해 input 값이 담긴 DTO
             HttpServletRequest request) // 인증정보를 가진 request
-            throws io.openvidu.java.client.OpenViduJavaClientException, io.openvidu.java.client.OpenViduHttpException {
+            throws io.openvidu.java.client.OpenViduJavaClientException, io.openvidu.java.client.OpenViduHttpException
+    {
 
         // 토큰 유효성 검증
         Member auth_member = authorizeToken(request);
@@ -426,14 +427,16 @@ public class GameRoomService {
         // 2. git bash로 리눅스 언어, docker를 사용하여 OpenVidu를 설정 및 구축함
 
         // 현재 구축해놓은 OpenVidu 서버 주소
-        String OPENVIDU_URL = "https://cheiks.shop";
+        String OPENVIDU_URL = "https://openvidu.haetae.shop";
         // 서버 주소를 사용하기 위한 SECRET키
         String OPENVIDU_SECRET = "MY_SECRET";
 
         // OpenVidu 객체에 주소와 SECRET키를 넣어 사용 준비
         OpenVidu openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+
         // 화상채팅을 사용하려면 session을 사용하게 되는데, session 설정값을 가져옴.
         SessionProperties properties = new SessionProperties.Builder().build();
+
 
         // OpenVidu에 세션 설정값을 input 하여 session 생성
         Session session = openvidu.createSession(properties);
@@ -446,6 +449,7 @@ public class GameRoomService {
                 .build();
         // 빌드된 세션 설정 값으로 세션의 연결점(커넥션) 생성
         Connection connection = session.createConnection(connectionProperties);
+
         // 커넥션을 사용해 token 생성 (session 과 마찬가지로 OpenVidu 컨텐츠를 사용하기 위해 token도 필요함)
         String token = connection.getToken();
 
