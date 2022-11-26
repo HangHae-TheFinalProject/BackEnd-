893288f67de5a09c26819746d21ccb2920657db2
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-11-26 19:22:35 +0900

    Feat : Member - 회원탈퇴 기능 추가

--- src/main/java/com/example/finalproject/controller/MemberController.java
+++ src/main/java/com/example/finalproject/controller/MemberController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -53,5 +50,12 @@ public class MemberController {
         return memberService.logout(request);
     }
 
+
+    // 회원탈퇴
+    @DeleteMapping("/signout")
+    public ResponseEntity<PrivateResponseBody> signout(
+            HttpServletRequest request){
+        return memberService.signout(request);
+    }
 
 }
