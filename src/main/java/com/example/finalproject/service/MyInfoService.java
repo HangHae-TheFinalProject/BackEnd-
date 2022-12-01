dfaa4d06069f3c94629215535bb6348533ccb164
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-12-02 01:53:14 +0900

    Fix : MyInfo - 마이페이지 전적 조회 로직 업데이트

--- src/main/java/com/example/finalproject/service/MyInfoService.java
+++ src/main/java/com/example/finalproject/service/MyInfoService.java
@@ -3,6 +3,7 @@ package com.example.finalproject.service;
 import com.example.finalproject.controller.response.PostResponseDto;
 import com.example.finalproject.controller.response.RewardResponseDto;
 import com.example.finalproject.domain.Member;
+import com.example.finalproject.domain.MemberActive;
 import com.example.finalproject.domain.Post;
 import com.example.finalproject.domain.Reward;
 import com.example.finalproject.exception.PrivateException;
@@ -16,12 +17,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import static com.example.finalproject.domain.QPost.post;
+import static com.example.finalproject.domain.QMemberActive.memberActive;
 
 @RequiredArgsConstructor
 @Service
@@ -29,6 +32,7 @@ public class MyInfoService {
 
     private final TokenProvider tokenProvider;
     private final JPAQueryFactory jpaQueryFactory;
+    private final EntityManager em;
 
 
     // 인증 정보 검증 부분을 한 곳으로 모아놓음
@@ -78,24 +82,66 @@ public class MyInfoService {
     }
 
 
-    // 모든 전적 조회
+    // 모든 전적 및 닉네임 조회
+    @Transactional
     public ResponseEntity<PrivateResponseBody> getMyAllRecord(HttpServletRequest request) {
         // 인증된 유저 정보
         Member auth_member = authorizeToken(request);
+
+        MemberActive userActive = jpaQueryFactory
+                .selectFrom(memberActive)
+                .where(memberActive.member.eq(auth_member))
+                .fetchOne();
+
+        if(userActive.getPlayminute() >= 60){
+            Long overminute = userActive.getPlayminute() / 60;
+            Long remainminute = userActive.getPlayminute() - (overminute * 60);
+
+            jpaQueryFactory
+                    .update(memberActive)
+                    .set(memberActive.playhour, userActive.getPlayhour() + overminute)
+                    .set(memberActive.playminute, remainminute)
+                    .where(memberActive.member.eq(auth_member))
+                    .execute();
+
+            em.flush();
+            em.clear();
+        }
+
+        String totalPlayTime = "";
+
+        if(userActive.getPlayhour() == 0L && userActive.getPlayminute() == 0L){
+            totalPlayTime = "아직 플레이하지 않았습니다.";
+        }else if(userActive.getPlayhour() == 0L && userActive.getPlayminute() != 0L){
+            totalPlayTime = userActive.getPlayminute() + "분";
+        }else if(userActive.getPlayhour() != 0L && userActive.getPlayminute() == 0L){
+            totalPlayTime = userActive.getPlayhour() + "시간";
+        }else if(userActive.getPlayhour() != 0L && userActive.getPlayminute() != 0L){
+            totalPlayTime = userActive.getPlayhour() + "시간 " + userActive.getPlayminute() + "분";
+        }
+
+        Integer rewardCnt = 0;
+
+        if(!auth_member.getRewards().isEmpty()){
+            rewardCnt = auth_member.getRewards().size();
+        }
+
 
         // 전적 저장
         HashMap<String, String> allRecordSet = new HashMap<>();
 
         allRecordSet.put("nickname", auth_member.getNickname()); // 닉네임
-        allRecordSet.put("allPlayRecord", Long.toString(auth_member.getWinNum() + auth_member.getLossNum())); // 총 플레이 수
-        allRecordSet.put("allLierPlayRecord", Long.toString(auth_member.getWinLIER() + auth_member.getLossLIER())); // 총 라이어 플레이 수
-        allRecordSet.put("allCitizenPlayRecord", Long.toString(auth_member.getWinCITIZEN() + auth_member.getLossCITIZEN())); // 총 시민 플레이 수
-        allRecordSet.put("winNum", Long.toString(auth_member.getWinNum())); // 전체 승리 수
-        allRecordSet.put("lossNum", Long.toString(auth_member.getLossNum())); // 전체 패배 수
-        allRecordSet.put("winLIER", Long.toString(auth_member.getWinLIER())); // 라이어로 승리한 수
-        allRecordSet.put("loseLIER", Long.toString(auth_member.getLossLIER())); // 라이어로 패배한 수
-        allRecordSet.put("winCITIZEN", Long.toString(auth_member.getWinCITIZEN())); //시민으로 승리한 수
-        allRecordSet.put("loseCITIZEN", Long.toString(auth_member.getLossCITIZEN())); // 시민으로 패배한 수
+        allRecordSet.put("allPlayRecord", auth_member.getWinNum() + auth_member.getLossNum() + "전 " + auth_member.getWinNum() + "승 " + auth_member.getLossNum() + "패"); // 총 플레이 수
+//        allRecordSet.put("allLierPlayRecord", Long.toString(auth_member.getWinLIER() + auth_member.getLossLIER())); // 총 라이어 플레이 수
+//        allRecordSet.put("allCitizenPlayRecord", Long.toString(auth_member.getWinCITIZEN() + auth_member.getLossCITIZEN())); // 총 시민 플레이 수
+//        allRecordSet.put("winNum", Long.toString(auth_member.getWinNum())); // 전체 승리 수
+//        allRecordSet.put("lossNum", Long.toString(auth_member.getLossNum())); // 전체 패배 수
+        allRecordSet.put("winLIER", auth_member.getWinLIER() + "승"); // 라이어로 승리한 수
+//        allRecordSet.put("loseLIER", Long.toString(auth_member.getLossLIER())); // 라이어로 패배한 수
+        allRecordSet.put("winCITIZEN", auth_member.getWinCITIZEN() + "승"); //시민으로 승리한 수
+//        allRecordSet.put("loseCITIZEN", Long.toString(auth_member.getLossCITIZEN())); // 시민으로 패배한 수
+        allRecordSet.put("totalPlayTime", totalPlayTime); // 총 플레이 시간
+        allRecordSet.put("rewardCnt", rewardCnt + "개"); // 보유 리워드 개수
 
         return new ResponseEntity<>(new PrivateResponseBody(StatusCode.OK, allRecordSet), HttpStatus.OK);
     }
