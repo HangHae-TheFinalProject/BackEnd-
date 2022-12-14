244ea493e1b1317a703f0a2a886f5783ca080548
JayEsEichi <96898059+JayEsEichi@users.noreply.github.com>
2022-12-14 19:29:27 +0900

    동적쿼리 테스트 중

--- src/main/java/com/example/finalproject/repository/DynamicQueryDsl.java
+++ src/main/java/com/example/finalproject/repository/DynamicQueryDsl.java
@@ -1,6 +1,7 @@
 package com.example.finalproject.repository;
 
 import com.example.finalproject.domain.GameRoom;
+import com.example.finalproject.domain.GameRoomMember;
 import com.example.finalproject.domain.Mode;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -10,17 +11,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import static com.example.finalproject.domain.QGameRoom.gameRoom;
+import static com.example.finalproject.domain.QGameRoomMember.gameRoomMember;
 
 @RequiredArgsConstructor
 @Repository
 public class DynamicQueryDsl {
     private final JPAQueryFactory jpaQueryFactory;
 
+    // 게임방 조회
     public List<GameRoom> findGameRooms(String condition) {
         return jpaQueryFactory
                 .selectFrom(gameRoom)
                 .where(eqMode(condition))
-//                .orderBy(gameRoom.createdAt.asc())
                 .orderBy(gameRoom.createdAt.desc())
                 .fetch();
     }
